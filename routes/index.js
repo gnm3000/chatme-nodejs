9f76f5232c35707da60e4ec0b2b844b38d7dc7fe
German Montenegro <gnm3000@gmail.com>
2013-05-22 01:35:49 -0300

    se oculta edad

--- routes/index.js
+++ routes/index.js
@@ -192,6 +192,62 @@ if(ciudad!=''){conditions.push({ciudad:ciudad})}
 
 console.log("conditions="+JSON.stringify(conditions));
 var edad = req.query["edad"];
+
+//
+//calcular la edad de una persona 
+//recibe la fecha como un string en formato español 
+//devuelve un entero con la edad. Devuelve false en caso de que la fecha sea incorrecta o mayor que el dia actual 
+var calcular_edad = function(fecha){ 
+
+    //calculo la fecha de hoy 
+    hoy=new Date() 
+    //alert(hoy) 
+
+    //calculo la fecha que recibo 
+    //La descompongo en un array 
+    var array_fecha = fecha.split("/") 
+    //si el array no tiene tres partes, la fecha es incorrecta 
+    if (array_fecha.length!=3) 
+         return false 
+
+    //compruebo que los ano, mes, dia son correctos 
+    var ano 
+    ano = parseInt(array_fecha[2]); 
+    if (isNaN(ano)) 
+         return false 
+
+    var mes 
+    mes = parseInt(array_fecha[0]); 
+    if (isNaN(mes)) 
+         return false 
+
+    var dia 
+    dia = parseInt(array_fecha[1]); 
+    if (isNaN(dia)) 
+         return false 
+
+
+    //si el año de la fecha que recibo solo tiene 2 cifras hay que cambiarlo a 4 
+    if (ano<=99) 
+         ano +=1900 
+
+    //resto los años de las dos fechas 
+    edad=hoy.getYear()- ano - 1; //-1 porque no se si ha cumplido años ya este año 
+
+    //si resto los meses y me da menor que 0 entonces no ha cumplido años. Si da mayor si ha cumplido 
+    if (hoy.getMonth() + 1 - mes < 0) //+ 1 porque los meses empiezan en 0 
+         return edad 
+    if (hoy.getMonth() + 1 - mes > 0) 
+         return edad+1 
+
+    //entonces es que eran iguales. miro los dias 
+    //si resto los dias y me da menor que 0 entonces no ha cumplido años. Si da mayor o igual si ha cumplido 
+    if (hoy.getUTCDate() - dia >= 0) 
+         return edad + 1 
+
+    return edad 
+} 
+//
 //if(conditions.length==0){conditions=null};
 conditions = {'$and':conditions};
 console.log("conditions="+JSON.stringify(conditions));
@@ -204,7 +260,7 @@ var callback1 = function(db,items,members,configuration){
     //valorFechaTermino = valorFecha -  ( 1 * 1 * 1 * 60 * 1000 ),
 
     fechaTermino = new Date(valorFechaTermino) // nuevo objeto de fecha: 20 de mayo - Thu May 20 2010 23:00:00 GMT-0400 (CLT)
-    console.log("la fecha es "+fechaTermino);
+    console.log("la fecha entre "+fechaTermino+" y "+new Date());
     
     var start_filter = {"$gte": fechaTermino, "$lt": new Date()};
    // var username_filter = {};
